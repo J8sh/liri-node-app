59eea8050f7d215064b5c85929fa0174ce971a0a
Josh <j8shigarcia@aol.com>
2017-09-09 09:32:24 -0500

    finnishing touches

---
+++ keys.js
@@ -0,0 +1,16 @@
+module.exports = {
+	twitterKeys: {
+		  consumer_key: 		'cwf0i0ifUuMpJskEbTzXGJDts',
+		  consumer_secret: 		'WlHMileGUfxGeZZPxvAslh8C7qUProgmpzbJZFkLEjP4dU7kE6',
+		  access_token_key: 	'905545297576808448-5OXxgXf4J8YBYCH8RLbfw2U4eMKOQYF',
+		  access_token_secret: 	'B7HTdaRcIlzIYJqG3hddbYTfS6zpOSZpsp1qzTTL2RYZG'
+		},
+	spotifyKeys: {
+			client_id: 		'17b2d3be0be746f4b88ae16e8da2bf4c',
+			client_secret: 	'28f2ec332a7244f882daf45299695d39'
+			}
+}
+
+//Client ID: 17b2d3be0be746f4b88ae16e8da2bf4c
+
+//Client Secret: 28f2ec332a7244f882daf45299695d39
